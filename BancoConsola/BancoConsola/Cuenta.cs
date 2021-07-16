89f4c3ae21887cc5d6ac0c6b1300b45dbb1b1d70
Tarde <Tarde@ASATA408>
2021-07-16 16:55:19 +0200

    Insertados constructores

---
+++ BancoConsola/BancoConsola/Cuenta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoConsola
+{
+    public class Cuenta
+    {
+        //Declaramos los campos privados
+        private int _NumeroCuenta;
+        private string _NombreCliente;
+        private double _Saldo;
+
+        //Contructor por defecto
+        public Cuenta() 
+        {
+            
+        }
+        //Constructor con parametros
+        public Cuenta(int NumeroCuenta, string NombreCliente, double Saldo) 
+        {
+            this._NumeroCuenta = NumeroCuenta;
+            this._NombreCliente = NombreCliente;
+            this._Saldo = Saldo;
+        }
+
+        //getters y setters
+
+        public int PNumeroCuenta 
+        {
+            get { return _NumeroCuenta; } set { _NumeroCuenta = value; }
+        }
+
+        public string PNombreCliente 
+        {
+            get { return _NombreCliente;}
+            set { _NombreCliente = value; }
+        }
+
+        public double PSaldo 
+        {
+            get { return _Saldo; }
+            set { _Saldo = value; }
+        }
+
+
+    }
+}
