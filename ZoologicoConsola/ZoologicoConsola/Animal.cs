467e6a2c6d7819c31dc1b9752bf74c6177e3efcc
Tarde <Tarde@ASATA408>
2021-07-16 18:17:15 +0200

    Definida Clase Animal

---
+++ ZoologicoConsola/ZoologicoConsola/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoologicoConsola
+{
+   public class Animal
+    {
+        //Todos sus campos privados
+        private string _especie;
+        private string _nombre;
+        private double _peso;
+        private int _jaula;
+
+        //constructor por defecto 
+        public Animal()
+        {
+        }
+        //constructor con parámetros
+        public Animal(string especie, string nombre,double peso, int jaula)
+        {
+            this._especie = especie;
+            this._nombre = nombre;
+            this._peso = peso;
+            this._jaula = jaula;
+        }
+
+        //get y set
+        public string Pespecie
+        {
+            get { return _especie; }
+            set { _especie = value; }
+        }
+
+        public string Pnombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public double Ppeso
+        {
+            get { return _peso; }
+            set { _peso = value; }
+        }
+
+
+        public int Pjaula
+        {
+            get { return _jaula; }
+            set { _jaula = value; }
+        }
+    
+        public void QueClaseDeAnimalEres()
+        {
+            Console.WriteLine("Especie: " + _especie + "\nNombre: " + _nombre + "\nPeso: " + _peso + "\n Nº Jaula: " + _jaula);
+        }
+    }
+}
