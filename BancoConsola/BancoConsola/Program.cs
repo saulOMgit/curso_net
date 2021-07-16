9b783bd1ae85a7b20675d0153881b016485f57e7
Tarde <Tarde@ASATA408>
2021-07-16 17:29:44 +0200

    Programa terminado a revisar

--- BancoConsola/BancoConsola/Program.cs
+++ BancoConsola/BancoConsola/Program.cs
@@ -13,8 +13,45 @@ namespace BancoConsola
         static void Main(string[] args)
         {
             Cuenta C1 = new Cuenta(007,"Frodo",1000);
-            C1.Depositar();
-            C1.ConsultarDatos();
+            int opcion=0;
+            do
+            {
+                Console.WriteLine("1.Crear Cuenta\n2.Depositar una cantidad\n3.Retirar Dinero\n4.Consultar Saldo\n5.Salir de la App");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion) 
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Introduce número de cuenta:");
+                        C1.PNumeroCuenta = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce Nombre del titular:");
+                        C1.PNombreCliente = Console.ReadLine();
+                        Console.WriteLine("Introduce sueldo inicial");
+                        C1.PSaldo = double.Parse(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.Clear();
+                        C1.Depositar();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        C1.Retirar();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        C1.ConsultarDatos();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Que tenga un buen dia");
+                        break;
+                    default:
+                        Console.WriteLine("Valor incorrecto");
+                        break;
+                }   
+            } while (opcion != 5);
+
             Console.ReadLine();
         }
     }
