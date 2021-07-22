8bd1e049b0427682659e1e7d9a458012bdd7a41c
Tarde <Tarde@ASATA408>
2021-07-22 20:14:19 +0200

    DIA 1

--- EjercicioRepasoLunesLINQ/EjercicioRepasoLunesLINQ/Form1.Designer.cs
+++ EjercicioRepasoLunesLINQ/EjercicioRepasoLunesLINQ/Form1.Designer.cs
@@ -168,6 +168,7 @@ namespace EjercicioRepasoLunesLINQ
             this.btnSalir.TabIndex = 12;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // Form1
             // 
--- EjercicioRepasoLunesLINQ/EjercicioRepasoLunesLINQ/Form1.cs
+++ EjercicioRepasoLunesLINQ/EjercicioRepasoLunesLINQ/Form1.cs
@@ -16,5 +16,10 @@ namespace EjercicioRepasoLunesLINQ
         {
             InitializeComponent();
         }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
