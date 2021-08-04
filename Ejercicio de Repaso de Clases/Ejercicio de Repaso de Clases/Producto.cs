70a99407a99969674ceb88bede592eca1405b631
Tarde <Tarde@ASATA408>
2021-08-04 20:22:57 +0200

    TO DO: finish menu

---
+++ Ejercicio de Repaso de Clases/Ejercicio de Repaso de Clases/Producto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_Repaso_de_Clases
+{
+    class Producto
+    {
+        //campos privados
+        private string _nombre;
+        private int _precio;
+        private string _categoria;
+        
+        //constructor por defecto
+        public Producto() 
+        {        
+        }
+        //constructor con parámetros
+        public Producto(string nombre, int precio, string categoria) 
+        {
+            this._nombre = nombre;
+            this._precio = precio;
+            this._categoria = categoria;
+        }
+        public Producto(string nombre, int precio) 
+        {
+            this._nombre = nombre;
+            this._precio = precio;
+        }
+
+        //get y set
+        public string Pnombre 
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public int Pprecio 
+        {
+            get { return _precio; }
+            set { _precio = value; }
+        }
+        public string Pcategoria 
+        {
+            get { return _categoria; }
+            set { _categoria = value; }
+        }
+
+        public void Mostrar() 
+        {
+            Console.WriteLine("Nombre: " + _nombre + "\nPrecio: " + _precio + "\nCategoria: " + _categoria);
+        }
+
+    }
+}
---
+++ Ejercicio de Repaso de Clases/Ejercicio de Repaso de Clases/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_Repaso_de_Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto producto1 = new Producto("Guiller", 666, "Melenudo");
+            Producto producto2 = new Producto("La Obituarios",1000000);
+
+            string opcion;
+
+            producto1.Mostrar();
+            producto2.Mostrar();
+            Console.ReadKey();
+
+            do
+            {
+                Console.WriteLine("1.Crear Objeto\n2.Hacer Pedido\n3.Mostrar el total+IVA\n4.Salir");
+                opcion = Console.ReadLine();
+                Console.Clear();
+
+                switch (opcion) 
+                {
+                    case "1":
+                        Console.WriteLine("introduce nombre de producto: ");
+                        producto2.Pnombre = Console.ReadLine();
+                        Console.WriteLine("Introduce precio: ");
+                        producto2.Pprecio = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce categoria");
+                        producto2.Pcategoria = Console.ReadLine();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine("Valor no valido");
+                        break;
+
+                }
+                    
+            } while (opcion!="4");
+        }
+    }
+}
