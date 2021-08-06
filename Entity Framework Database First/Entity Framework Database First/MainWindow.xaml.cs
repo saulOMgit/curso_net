5668d6ed7f83dabccd9ea3444e413e1da3150d3c
Tarde <Tarde@ASATA408>
2021-08-06 20:01:54 +0200

    Botones Ventana Principal : Hechos

--- Entity Framework Database First/Entity Framework Database First/MainWindow.xaml.cs
+++ Entity Framework Database First/Entity Framework Database First/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace Entity_Framework_Database_First
     /// </summary>
     public partial class MainWindow : Window
     {
+        EscuelaEntities DbEntityes = new EscuelaEntities();
+        public static DataGrid ControlDatagrid;
+
         public MainWindow()
         {
+            //Cargamos el Datagrid con los datos
             InitializeComponent();
+            MyDG.ItemsSource = DbEntityes.Profesores.ToList();
+            ControlDatagrid = MyDG;
+        }
+
+        private void MyDG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //el primer botón llama al cuadro Insertar
+            VentanaInsertar Vinsertar = new VentanaInsertar();
+            Vinsertar.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //El segundo botón llama al cuadro modificar
+            //int id = (MyDG.SelectedItem as Profesores).Id;
+            VentanaModificar Vcambiar = new VentanaModificar();
+            Vcambiar.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //eliminar
+            //int id = (MyDG.SelectedItem as Profesores).Id;
+            var deleteProfe = DbEntityes.Profesores.Where(m =>
+              m.Id == id).Single();
+            DbEntityes.Profesores.Remove(deleteProfe);
+            DbEntityes.SaveChanges();
+            ControlDatagrid.ItemsSource =
+                DbEntityes.Profesores.ToList();
         }
     }
 }
