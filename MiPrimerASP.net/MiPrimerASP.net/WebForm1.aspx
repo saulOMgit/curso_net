d35290b740963b4791cc9bf99aa916e7ee4effd7
Tarde <Tarde@ASATA408>
2021-08-25 20:23:46 +0200

    Finalizado

---
+++ MiPrimerASP.net/MiPrimerASP.net/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="MiPrimerASP.net.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        #Select1 {
+            width: 82px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="1"></asp:Label>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Resultado" OnClick="Button1_Click" />
+        </p>
+        <asp:Label ID="Label2" runat="server" Text="Ingrese primer valor"></asp:Label><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+        <asp:Label ID="Label3" runat="server" Text="Ingrese segundo valor"></asp:Label><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+<%--        <select id="Select1" runat="server">
+            <option>sumar</option>
+            <option>restar</option>
+        </select>--%>
+<%--        <asp:RadioButton ID="RadioButton1" runat="server" Text="sumar" GroupName="operacion"/><br />
+        <asp:RadioButton ID="RadioButton2" runat="server" Text="restar" GroupName="operacion"/><br />--%>
+                <br />
+        <asp:ListBox ID="ListBox1" runat="server">
+            <asp:ListItem>sumar</asp:ListItem>            
+            <asp:ListItem>restar</asp:ListItem>
+            <asp:ListItem>multiplicar</asp:ListItem>           
+            <asp:ListItem>dividir</asp:ListItem>
+        </asp:ListBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+    </form>
+    
+</body>
+</html>
---
+++ MiPrimerASP.net/MiPrimerASP.net/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiPrimerASP.net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Label1.Text = DateTime.Today.ToString("d");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(Label1.Text);
+            x++;Label1.Text = x.ToString();
+
+            if (ListBox1.Items[0].Selected)
+            {
+                int resultado;
+                int x1, x2;
+                x1 = int.Parse(TextBox1.Text);
+                x2 = int.Parse(TextBox2.Text);
+                resultado = x1 + x2;
+                Label4.Text = "La suma de los dos valores es:" + resultado;
+            }
+            else if (ListBox1.Items[1].Selected)
+            {
+                int resultado;
+                int x1, x2;
+                x1 = int.Parse(TextBox1.Text);
+                x2 = int.Parse(TextBox2.Text);
+                resultado = x1 - x2;
+                Label4.Text = "La resta de los dos valores es:" + resultado;
+            }
+            else if (ListBox1.Items[2].Selected)
+            {
+                int resultado;
+                int x1, x2;
+                x1 = int.Parse(TextBox1.Text);
+                x2 = int.Parse(TextBox2.Text);
+                resultado = x1 * x2;
+                Label4.Text = "La multiplicación de los dos valores es:" + resultado;
+            }
+            else if (ListBox1.Items[3].Selected)
+            {
+                int resultado;
+                int x1, x2;
+                x1 = int.Parse(TextBox1.Text);
+                x2 = int.Parse(TextBox2.Text);
+                resultado = x1 / x2;
+                Label4.Text = "La división de los dos valores es:" + resultado;
+            }
+        }
+       
+    }
+}
